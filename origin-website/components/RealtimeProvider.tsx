0ab46670a692c9c3a8e713aa8087bb9e6dad806c
kentlee9099 <kentlee9099@gmail.com>
2026-06-21 05:13:26 +0800

    Delete origin-website directory

--- origin-website/components/RealtimeProvider.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import { useRealtimeSync } from '@/hooks/useRealtimeSync'
-export function RealtimeProvider({ children }: { children: React.ReactNode }) {
-  useRealtimeSync()
-  return <>{children}</>
-}
